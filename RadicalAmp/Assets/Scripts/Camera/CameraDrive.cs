c703ac1bf6fc3f769cb8be45582f2d06bd9a5d47
Niklas <niklas.hassmann@gmail.com>
2019-08-19 12:35:38 +0200

    CameraZoomWiP

--- RadicalAmp/Assets/Scripts/Camera/CameraDrive.cs
+++ RadicalAmp/Assets/Scripts/Camera/CameraDrive.cs
@@ -3,27 +3,87 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public class CameraDrive : MonoBehaviour
+public class CameraDrive : Singleton
 {
 
-    public float smooth = 0.3f, height, posZminus;
+    public float smooth = 0.3f;
+    [Range(2, 10)]
+    public float height, posZminus;
 
     private Transform player;
 
     private Vector3 velocity = Vector3.zero;
 
+    [SerializeField] float zoomInDelay = 5;
+    public bool zoomInReady = false;
+
+    public bool setZoomedOut;
+    public bool setZoomedIn;
+
     private void Start()
     {
         player = GameObject.FindWithTag("Player").GetComponent<Transform>();
+        ZoomIn();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (gameObject.GetComponent<CameraRangeAdjust>().CheckForEnemiesInRange())
+        {
+            ZoomOut();
+        }
+        else if(gameObject.GetComponent<CameraRangeAdjust>().CheckForEnemiesInRange() == false && zoomInReady)
+        {
+            ZoomIn();
+        }
+        
         Vector3 pos = new Vector3();
         pos.x = player.position.x;
         pos.z = player.position.z - posZminus;
         pos.y = player.position.y + height;
         transform.position = Vector3.SmoothDamp(transform.position, pos, ref velocity, smooth);
+
+        if (setZoomedIn)
+        {
+            ZoomIn();
+            return;
+        }
+
+        else if (setZoomedOut)
+        {
+            ZoomOut();
+            return;
+        }
+    }
+
+    public void ZoomOut()
+    {
+        Debug.Log("ZoomingOut");
+
+       
+        height = 8;
+        posZminus = 11;
+        zoomInReady = false;
+
+        if (!setZoomedOut)
+        {
+            StartCoroutine(ZoomInDelay(zoomInDelay));
+        }
+    }
+
+    public void ZoomIn()
+    {
+     
+        height = 5;
+        posZminus = 6;
+    }
+
+    IEnumerator ZoomInDelay(float waittime)
+    {
+        yield return new WaitForSeconds(waittime);
+
+
+        zoomInReady = true;
     }
 }
---
+++ RadicalAmp/Assets/Scripts/Camera/CameraRangeAdjust.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRangeAdjust : MonoBehaviour
+{
+    [SerializeField] GameObject player;
+    [Range(0,10)]
+    [SerializeField] float innerRange = 5;
+    [Range(5,20)]
+    [SerializeField] float outerRange = 10;
+    [SerializeField] int layerID = 13;
+
+    private int layerMask;
+    
+
+    [SerializeField] bool debugInnerSphere, debugOuterSphere;
+
+    [SerializeField] Collider[] innerEnemies;
+    [SerializeField] Collider[] outerEnemies;
+
+    [SerializeField] int zoomTriggerAmount = 3; // enemies Needed to zoom out
+
+
+    private void Start()
+    {
+         layerMask = 1 << layerID;
+    }
+
+    public void FixedUpdate()
+    {
+        CheckForEnemiesInRange();
+    }
+
+    /// <summary>
+    /// Sphere Debug
+    /// </summary>
+    private void OnDrawGizmos()
+    {
+        if (debugInnerSphere)
+        {
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawSphere(player.transform.position, innerRange);
+        }
+
+        if (debugOuterSphere)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawSphere(player.transform.position, outerRange);
+        }
+    }
+
+
+    /// <summary>
+    /// Comparing inner cirlce vs outer circle
+    /// </summary>
+    /// <returns></returns>
+    public bool CheckForEnemiesInRange()
+    {
+        innerEnemies = Physics.OverlapSphere(player.transform.position, innerRange, layerMask);
+
+        outerEnemies = Physics.OverlapSphere(player.transform.position, outerRange, layerMask);
+
+        if (outerEnemies.Length - innerEnemies.Length > zoomTriggerAmount)
+        {
+            return true;
+        }
+
+        else
+        {
+            return false;
+        }
+    }
+
+}

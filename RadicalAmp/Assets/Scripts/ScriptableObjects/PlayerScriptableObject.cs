ca0fafc7b557badfa492cc34cbccb9b0c0ad0105
Benjamin Rose <rose.benjamin@t-online.de>
2019-08-20 09:41:33 +0200

    Benjamin#003 Created ScriptableObject Scripts for Characters, Boss and Player

---
+++ RadicalAmp/Assets/Scripts/ScriptableObjects/PlayerScriptableObject.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Player", menuName = "Characters/Player")]
+public class PlayerScriptableObject : CharacterScriptableObject
+{
+
+}

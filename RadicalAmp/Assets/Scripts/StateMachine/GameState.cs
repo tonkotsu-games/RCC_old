94eada7074c8d807531af286dd718194f5f14cd3
Benjamin Grill <benjamin.grill@ue-germany.de>
2019-08-20 16:22:43 +0200

    Benny#019 GameState start

---
+++ RadicalAmp/Assets/Scripts/StateMachine/GameState.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameState
+{
+    public enum State
+    {
+     Menue,
+     GameStart,
+     Play,
+     Pause,
+     Exit
+    }
+}

757e74000f3509c74fa0a41d9877a26d307370a0
Benjamin Rose <rose.benjamin@t-online.de>
2019-08-20 13:32:00 +0200

    Benjamin#006 Created Actor and Feedback Classes

---
+++ RadicalAmp/Assets/Scripts/StateMachine/Actor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Actor : MonoBehaviour
+{
+    private StateMachine stateMachine = new StateMachine();
+
+    public StateMachine StateMachine { get => stateMachine; private set => stateMachine = value; }
+
+    [SerializeField]
+    private CharacterScriptableObject actorData;
+
+    public CharacterScriptableObject ActorData { get => actorData; private set => actorData = value; }
+
+    private void Update()
+    {
+        StateMachine.StateExecuteTick();
+    }
+}

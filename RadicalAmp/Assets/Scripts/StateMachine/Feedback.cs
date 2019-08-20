43e21a0317c1fd3a3b111f0340290786f1b9d851
Benjamin Rose <rose.benjamin@t-online.de>
2019-08-20 13:32:00 +0200

    Created Actor and Feedback Classes

---
+++ RadicalAmp/Assets/Scripts/StateMachine/Feedback.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Feedback : MonoBehaviour
+{
+    private CharacterScriptableObject actorData;
+
+    private void Start()
+    {
+        actorData = this.gameObject.GetComponent<Actor>().ActorData;
+
+        if(actorData != null)
+        {
+            Debug.Log("Data on Feedback found on " + this.gameObject.name); 
+        }
+        else
+        {
+            Debug.LogError("Data on Feedback NOT found on " + this.gameObject.name);
+        }
+    }
+}

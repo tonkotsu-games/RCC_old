8d7555648c14f4ec26574ea809ac1cb0d4056a02
Niklas <niklas.hassmann@gmail.com>
2019-08-23 18:08:43 +0200

    Starting to move stages

---
+++ RadicalAmp/Assets/Scripts/LevelStructure/LevelManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    [Tooltip("The amount of units a ring moves down")]
+    public float ringStepSize = 5;
+    public float ringMoveSpeed = 1;
+    public StageRingController[] rings;
+
+
+    void Start()
+    {
+        rings = GetComponentsInChildren<StageRingController>();
+        StageRingController.stepSize = ringStepSize;
+        StageRingController.moveSpeed = ringMoveSpeed;
+        Debug.Log("StartingTimer");
+        StartCoroutine("StageChangeTest");
+    }
+
+    IEnumerator StageChangeTest()
+    {
+        int i = 0;
+        while (i <= 4)
+        {
+            Debug.Log("Round: " + i + " -> starting countdown");
+            yield return new WaitForSeconds(10);
+            Debug.Log("setting " + rings[i].gameObject.name + "'s isMoving to true");
+
+            switch (i)
+            {
+                case 1:
+                    //Move only Ring4
+                    break;
+                case 2:
+                    //Move Ring 4 & 3
+                    break;
+                case 3:
+                    //Move Ring 4 & 3 & 2
+                    break;
+                case 4:
+                    //Move all rings
+                    break;
+            }
+
+            rings[i].isMoving = true;
+            i++;
+        }
+    }
+
+}
---
+++ RadicalAmp/Assets/Scripts/LevelStructure/StageRingController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageRingController : MonoBehaviour
+{
+    public int ringNumber;
+
+    public static float stepSize;
+    public static float moveSpeed;
+    public bool isMoving;
+
+    private Vector3 basePosition;
+
+    private void Start()
+    {
+        transform.position = basePosition;
+    }
+
+    public void Update()
+    {
+        if (isMoving)
+        {
+            MoveDown();
+        }
+        
+    }
+    public void MoveDown()
+    {
+        if (transform.position.y >= basePosition.y - stepSize)
+        {
+            transform.Translate(0,-moveSpeed * Time.deltaTime , 0);
+        }
+        else
+        {
+            isMoving = false;
+        }
+    }
+}

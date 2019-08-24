9201214f8a45d5590bf84d1bfbef06009405c500
Benjamin Rose <rose.benjamin@t-online.de>
2019-08-24 18:57:04 +0200

    Benjamin#026 Awesome Camera started!

--- RadicalAmp/Assets/Scripts/Camera/CameraTesting.cs
+++ RadicalAmp/Assets/Scripts/Camera/CameraTesting.cs
@@ -5,34 +5,53 @@ using UnityEngine;
 public class CameraTesting : MonoBehaviour
 {
     [SerializeField] Transform target;
-    [SerializeField] Vector3 offset;
+    Vector3 offsetCurrent;
+    [SerializeField] Vector3 offsetZero;
+    [SerializeField] Vector3 offsetOne;
+    [SerializeField] Vector3 offsetTwo;
     Vector3 newPosition = new Vector3(0,0,0);
+    [SerializeField] private int cameraState = 0;
     
     void Start()
     {
-        if(offset == new Vector3(0,0,0))
-        {
-            Debug.LogError("offset not setup on Camera!");
-        }
-
-        this.gameObject.transform.position = target.position + offset;
+        this.gameObject.transform.position = target.position + offsetZero;
+        offsetCurrent = offsetZero;
         transform.LookAt(target);
     }
 
 
     void Update()
     {
+        CalculateOffset();
         CalculateSmoothZFollow();
+        CalculateSmoothXFollow();
         ChangePosition();
     }
 
     private void CalculateSmoothZFollow()
     {
-        newPosition.z = ((target.transform.position.z + offset.z) - this.gameObject.transform.position.z) * 0.1f* Time.deltaTime;
+        newPosition.z = ((target.transform.position.z + offsetCurrent.z) - this.gameObject.transform.position.z) * 0.1f* Time.deltaTime * 25f;
+    }
+
+    private void CalculateSmoothXFollow()
+    {
+        newPosition.x = ((target.transform.position.x + offsetCurrent.x) - this.gameObject.transform.position.x) * 0.1f* Time.deltaTime * 25f;
     }
 
     private void ChangePosition()
     {
         this.gameObject.transform.position += newPosition;
+    }
+
+    private void CalculateOffset()
+    {
+        if(cameraState == 0)
+        {
+            offsetCurrent.z += (offsetZero.z - offsetCurrent.z) * 0.1f* Time.deltaTime * 25f;
+        }
+        else if(cameraState == 1)
+        {
+            offsetCurrent.z += (offsetOne.z - offsetCurrent.z) * 0.1f* Time.deltaTime * 25f;
+        }
     }
 }

08ab3c20b860656dcb0b486557eb83fafbf41289
Benjamin Rose <rose.benjamin@t-online.de>
2019-08-24 18:28:03 +0200

    Benjamin#025 fixed Player Dash

---
+++ RadicalAmp/Assets/Scripts/Camera/CameraTesting.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTesting : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] Vector3 offset;
+    Vector3 newPosition = new Vector3(0,0,0);
+    
+    void Start()
+    {
+        if(offset == new Vector3(0,0,0))
+        {
+            Debug.LogError("offset not setup on Camera!");
+        }
+
+        this.gameObject.transform.position = target.position + offset;
+        transform.LookAt(target);
+    }
+
+
+    void Update()
+    {
+        CalculateSmoothZFollow();
+        ChangePosition();
+    }
+
+    private void CalculateSmoothZFollow()
+    {
+        newPosition.z = ((target.transform.position.z + offset.z) - this.gameObject.transform.position.z) * 0.1f* Time.deltaTime;
+    }
+
+    private void ChangePosition()
+    {
+        this.gameObject.transform.position += newPosition;
+    }
+}

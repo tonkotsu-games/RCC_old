d0d2827e718185ba4c9d93d35f1eb930785d9f67
Benjamin Grill <benjamin.grill@ue-germany.de>
2019-08-27 16:49:06 +0200

    Benny#040 VFX and Particle Script

---
+++ RadicalAmp/Assets/Scripts/Particle/ParticleStartSize.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleStartSize : MonoBehaviour
+{
+    [Header("Particle System Prefab")]
+    [SerializeField] ParticleSystem particleSystem;
+    [Header("Particle System Multiplayer")]
+    [SerializeField] float particleMulti;
+    ParticleSystem.MainModule particleModule;
+
+
+    void Start()
+    {
+        particleModule = particleSystem.main;
+    }
+
+    void Update()
+    {
+        particleModule.startSizeMultiplier = particleMulti * juiceMeter.value;
+
+    }
+}
---
+++ RadicalAmp/Assets/Scripts/VFX/VFXIntesnity.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.VFX;
+
+
+public class VFXIntesnity : MonoBehaviour
+{
+    [Header("VFX Prefab")]
+    [SerializeField] VisualEffect vfx;
+    [Header("VFX Multiplayer")]
+    [SerializeField] float vfxMulti;
+    [Header("VFX emission Name")]
+    [SerializeField] string vfxName;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        vfx.SetFloat(vfxName, vfxMulti * juiceMeter.value);
+    }
+}

4c0ed18e4ebaae5230ca2f3475b0e6533fd73ad7
Niklas <niklas.hassmann@gmail.com>
2019-09-03 18:35:57 +0200

    Minor Adjustments left, MIGHT BREAK TUTORIAL
    Waiting for review

--- RadicalAmp/Assets/Scripts/Player/JuiceDash.cs
+++ RadicalAmp/Assets/Scripts/Player/JuiceDash.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.AI;
 
 public class JuiceDash : MonoBehaviour
 {
@@ -9,17 +10,31 @@ public class JuiceDash : MonoBehaviour
     Slider juiceMeter;
     BeatAnalyse beatAnalyse;
     public float juiceConsumedPerCharge = 30;
-    float range;
-    bool beat = false;
+    public float abilityRange = 20;
+    public float markerOffset = 5;
 
-    public enum ChargeStates { none, first, second,final,success,punish}
+    public GameObject playerClone;
+    //public int enemiesTargetedPerCharge = 3;
+    bool beat = false;
+    bool abilityReady = false;
+
+    Collider[] enemiesInRange;
+    LayerMask layerMask = 1 << 13;
+    public List<GameObject> markedTargets;
+    List<GameObject> activeMarkers;
+
+    //State Maschine
+    public enum ChargeStates { none, first, second, final, success, punish }
     ChargeStates currentState = ChargeStates.none;
     ChargeStates nextState = ChargeStates.first;
+
 
 
     // Start is called before the first frame update
     void Start()
     {
+        markedTargets = new List<GameObject>();
+        activeMarkers = new List<GameObject>();
         juiceMeter = Locator.instance.GetJuiceMeter();
         beatAnalyse = Locator.instance.GetBeat();
     }
@@ -27,40 +42,25 @@ public class JuiceDash : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (currentState != ChargeStates.final)
-        {
-            if (Input.GetButton("Attack"))
-            {
-                if (beatAnalyse.IsOnBeat(1000) && beat == false)
-                {
-                    beat = true;
-                    Debug.Log("Changing ChargeState");
-                    ChangeChargeState(nextState);
 
-                }
-                else if (!beatAnalyse.IsOnBeat(1000))
-                {
-                    beat = false;
-                }
-            }
-            else if (Input.GetButtonUp("Attack"))
+
+        //  if (currentState != ChargeStates.final)
+        //  {
+        if (abilityReady)
+        {
+            if (Input.GetButtonUp("Attack"))
             {
-                ChangeChargeState(ChargeStates.none);
+                ChangeChargeState(ChargeStates.success);
             }
         }
-        else
+        if (Input.GetButton("Attack"))
         {
-            if (Input.GetButtonUp("Attack") && beatAnalyse.IsOnBeat(1000) && beat == false)
+            if (beatAnalyse.IsOnBeat(1000) && beat == false)
             {
+                beat = true;
+                Debug.Log("Changing ChargeState");
                 ChangeChargeState(nextState);
-            }
-            else if (Input.GetButtonUp("Attack"))
-            {
-                ChangeChargeState(ChargeStates.punish);
-            }
-            else if (Input.GetButton("Attack") && beatAnalyse.IsOnBeat(1000) && beat == false)
-            {
-                ChangeChargeState(ChargeStates.punish);
+
             }
 
             else if (!beatAnalyse.IsOnBeat(1000))
@@ -68,6 +68,36 @@ public class JuiceDash : MonoBehaviour
                 beat = false;
             }
         }
+        // }
+        //else
+        //{
+        //   // if(beatAnalyse.IsOnBeat(1000)&&beat == false)
+        //   // {
+        //   //     ChangeChargeState(ChargeStates.success);
+        //   // }
+        //   // if (Input.GetButtonUp("Attack") && beatAnalyse.IsOnBeat(1000) && beat == false)
+        //   // {
+        //   //     Debug.Log("Using Juice Dash");
+        //   //     beat = true;
+        //   //     ChangeChargeState(nextState);
+        //   // }
+        //   // else if (Input.GetButtonUp("Attack"))
+        //   // {
+        //   //     Debug.Log("Punish because released button too early");
+        //   //     ChangeChargeState(ChargeStates.punish);
+        //   // }
+        //   // else if (Input.GetButton("Attack") && beatAnalyse.IsOnBeat(1000) && beat == false)
+        //   // {
+        //   //     Debug.Log("Punish because held too long");
+        //   //     beat = true;
+        //   //     ChangeChargeState(ChargeStates.punish);
+        //   // }
+        //   //
+        //   else if (!beatAnalyse.IsOnBeat(1000))
+        //   {
+        //       beat = false;
+        //   }
+        //}
 
     }
 
@@ -77,18 +107,36 @@ public class JuiceDash : MonoBehaviour
         {
             Debug.Log("already in state " + requestedState);
         }
-        else {
+        else
+        {
+
             currentState = requestedState;
+            Debug.Log("Current State: " + currentState);
 
             if (requestedState == ChargeStates.none)
             {
+                markedTargets.Clear();
+                abilityReady = false;
+                DisableAllMarkers();
                 Debug.Log("Charge failed");
                 nextState = ChargeStates.first;
             }
+
             else if (requestedState == ChargeStates.first)
             {
-                if (juiceMeter.value == 100)
+                if (juiceMeter.value >= 90)
                 {
+                    StartCoroutine(ReadyUpDelay());
+                    CollectEnemies();
+                    if (enemiesInRange.Length != 0)
+                    {
+                        markedTargets.Add(enemiesInRange[0].gameObject);
+                        DisplayMarkerOnTarget(markedTargets[0]);
+                        foreach (GameObject t in markedTargets)
+                        {
+                            t.gameObject.GetComponent<NavMeshAgent>().isStopped = true;
+                        }
+                    }
                     nextState = ChargeStates.second;
                     juiceMeter.value -= juiceConsumedPerCharge;
                 }
@@ -96,28 +144,145 @@ public class JuiceDash : MonoBehaviour
                 {
                     ChangeChargeState(ChargeStates.none);
                 }
-                
+
             }
-            else if(requestedState == ChargeStates.second)
+            else if (requestedState == ChargeStates.second)
             {
+                for (int i = 1; i <= 2; i++)
+                {
+                    // i == 2 i want to add the 3rd enemy in range  
+                    // 2 >= 2 enemies in range
+                    if (i <= enemiesInRange.Length - 1)
+                    {
+                        markedTargets.Add(enemiesInRange[i].gameObject);
+
+                    }
+                }
+
+                Debug.Log("Updating Markers");
+                foreach (GameObject t in markedTargets)
+                {
+                    DisplayMarkerOnTarget(t);
+                }
+                foreach (GameObject t in markedTargets)
+                {
+                    t.gameObject.GetComponent<NavMeshAgent>().isStopped = true;
+                }
                 nextState = ChargeStates.final;
                 juiceMeter.value -= juiceConsumedPerCharge;
             }
-            else if(requestedState == ChargeStates.final)
+
+
+            else if (requestedState == ChargeStates.final)
             {
+
+                for (int i = 3; i <= 5; i++)
+                {
+                    if (i <= enemiesInRange.Length - 1)
+                    {
+                        markedTargets.Add(enemiesInRange[i].gameObject);
+                    }
+                }
+
+
+                Debug.Log("Updating Markers");
+                foreach (GameObject t in markedTargets)
+                {
+                    DisplayMarkerOnTarget(t);
+                }
+                foreach (GameObject t in markedTargets)
+                {
+                    t.gameObject.GetComponent<NavMeshAgent>().isStopped = true;
+                }
+
+
                 nextState = ChargeStates.success;
                 juiceMeter.value -= juiceConsumedPerCharge;
             }
-            else if(requestedState == ChargeStates.success)
+            else if (requestedState == ChargeStates.success)
             {
+                nextState = ChargeStates.none;
+                SpawnClone(markedTargets[0]);
+                ChangeChargeState(nextState);
 
+            }
+            else if (requestedState == ChargeStates.punish)
+            {
+                ChangeChargeState(ChargeStates.none);
+            }
+
+        }
+    }
+
+    IEnumerator ReadyUpDelay()
+    {
+        yield return new WaitForSeconds(0.2f);
+        abilityReady = true;
+    }
+
+    void CollectEnemies()
+    {
+        enemiesInRange = Physics.OverlapSphere(transform.position, abilityRange, layerMask);
+
+    }
+
+    void DisplayMarkerOnTarget(GameObject target)
+    {
+        if (markedTargets.Count != 0)
+        {
+            var marker = Instantiate(GameObject.CreatePrimitive(PrimitiveType.Cube), target.transform.position + new Vector3(0, markerOffset, 0), Quaternion.identity);
+            marker.transform.parent = target.transform;
+            marker.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+            activeMarkers.Add(marker);
+        }
+    }
+    void DisableAllMarkers()
+    {
+        if (activeMarkers.Count != 0)
+        {
+            foreach (GameObject m in activeMarkers)
+            {
+                Destroy(m);
             }
         }
     }
 
-    void UseJuiceDash()
+
+    void SpawnClone(GameObject target)
     {
+        GameObject clone = Instantiate(playerClone, transform.position, Quaternion.identity);
+        clone.GetComponent<JuiceDashClone>().juiceDashScript = this;
+        clone.GetComponent<JuiceDashClone>().targets = new List<GameObject>(markedTargets);
+        clone.GetComponent<JuiceDashClone>().startAction = true;
+        TurnPlayerONandOff(false);
+
 
     }
-   
+
+    void TurnPlayerONandOff(bool value)
+    {
+        SkinnedMeshRenderer[] renderers = gameObject.GetComponentsInChildren<SkinnedMeshRenderer>();
+        foreach (SkinnedMeshRenderer rend in renderers)
+        {
+            rend.enabled = value;
+        }
+        gameObject.GetComponentInChildren<PlayerAttackCheck>().gameObject.SetActive(value);
+        gameObject.GetComponent<CapsuleCollider>().enabled = value;
+        gameObject.GetComponent<VFXIntensity>().enabled = value;
+        gameObject.GetComponent<PlayerController>().enabled = value;
+    }
+
+    public void BackToNormal()
+    {
+        TurnPlayerONandOff(true);
+        markedTargets.Clear();
+    }
+    private void OnGUI()
+    {
+        GUILayout.Label(currentState.ToString());
+        GUILayout.Label("Ability Ready: " + abilityReady);
+    }
 }
+    
+
+
---
+++ RadicalAmp/Assets/Scripts/Player/JuiceDashClone.cs
@@ -0,0 +1,144 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JuiceDashClone : MonoBehaviour
+{
+
+    public List<GameObject> targets;
+    public JuiceDash juiceDashScript;
+    Vector3 startPos;
+    List<GameObject> holograms;
+    public GameObject hologram;
+    [SerializeField] AnimationClip attackClip;
+    Animator anim;
+    bool dashing = false;
+    public bool startAction = false;
+    bool dashEnabled = false;
+    bool delayActive = false;
+    bool returned = false;
+    public float dashSpeed = 0.1f;
+    public float delay = 0.5f;
+    Vector3 dashDirection;
+    Vector3 nextPos;
+    int targetCounter = 0;
+    public GameObject dashParticlesPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+        anim = gameObject.GetComponent<Animator>();
+        holograms = new List<GameObject>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (!dashEnabled && startAction)
+        {
+            Debug.Log("Clone Calculating Position");
+            CalculateNextPos(targetCounter);
+            targetCounter++;
+        }
+        else if(dashEnabled)
+        {
+            if (!dashing)
+            {
+                dashing = true;
+                Debug.Log("enablingDashing");
+                SpawnDashParticles();
+                Physics.IgnoreLayerCollision(9, 13, true);
+            }
+
+            else
+            {
+                if (Vector3.Distance(transform.position, nextPos) >= 1)
+                {
+                    transform.position = Vector3.Lerp(transform.position, nextPos, dashSpeed*Time.deltaTime);
+                }
+                else
+                {
+                    if (!delayActive)
+                    {
+                        if (!returned)
+                        {
+                            GameObject holo = Instantiate(hologram, transform.position, Quaternion.identity);
+                            holograms.Add(holo);
+                        }
+                        anim.Play("Dashing");
+                        delayActive = true;
+                        StartCoroutine(DelayBetweenDashes());
+                    }
+                }
+            }
+        }
+    }
+
+    void CalculateNextPos(int counter)
+    {
+        if (counter < targets.Count)
+        {
+            nextPos = targets[counter].transform.position;
+            dashEnabled = true;
+        }
+        else if (counter == targets.Count)
+        {
+            returned = true;
+            nextPos = startPos;
+            dashEnabled = true;
+        }
+        else
+        {
+            startAction = false;
+            targetCounter = 0;
+            StartCoroutine(DelayBetweenKill());
+        }
+    }
+    private void SpawnDashParticles()
+    {
+        GameObject particlesInstance = Instantiate(dashParticlesPrefab, gameObject.transform.position, gameObject.transform.rotation) as GameObject;
+        particlesInstance.GetComponent<FollowPosition>().followTarget = gameObject.transform;
+        particlesInstance.gameObject.transform.Rotate(-90, 0, 0);
+        ParticleSystem parts = particlesInstance.GetComponentInChildren<ParticleSystem>();
+        float totalDuration = parts.main.duration + parts.main.startLifetime.constant + parts.main.startDelay.constant;
+        Destroy(particlesInstance, totalDuration);
+    }
+
+    IEnumerator DelayBetweenDashes()
+    {
+        Debug.Log("starting waittime");
+        yield return new WaitForSeconds(delay);
+        delayActive = false;
+        dashEnabled = false;
+        dashing = false;
+        Physics.IgnoreLayerCollision(9, 13, false);
+    }
+
+    IEnumerator DelayBetweenKill()
+    {
+        juiceDashScript.BackToNormal();
+        SkinnedMeshRenderer[] renderers = gameObject.GetComponentsInChildren<SkinnedMeshRenderer>();
+        foreach (SkinnedMeshRenderer rend in renderers)
+        {
+            rend.enabled = false;
+        }
+
+        int counter = 0;
+        while (counter < targets.Count)
+        {
+            yield return new WaitForSeconds(0.1f);
+            holograms[counter].GetComponent<Animator>().speed = 5;
+            holograms[counter].GetComponent<TutorialClone>().PlayAttack(true);
+            targets[counter].GetComponent<EnemyHP>().BloodSplat();
+            targets[counter].GetComponent<EnemyHP>().BloodSplat();
+            targets[counter].GetComponent<EnemyHP>().BloodSplat();
+            targets[counter].GetComponent<EnemyHP>().life = 0;
+            yield return new WaitForSeconds(attackClip.length*0.2f);
+            Destroy(holograms[counter]);
+            counter++;
+        }
+        Destroy(gameObject);
+
+    }
+}

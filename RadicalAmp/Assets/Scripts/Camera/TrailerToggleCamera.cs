abaca88aa414afbdc6854f46bf283c102ce15147
Benjamin Rose <rose.benjamin@t-online.de>
2019-09-03 12:15:45 +0200

    Benjamin#053 Started working on trailer camera

---
+++ RadicalAmp/Assets/Scripts/Camera/TrailerToggleCamera.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrailerToggleCamera : MonoBehaviour
+{
+    [SerializeField] GameObject mainCamera;
+    [SerializeField] GameObject trailerCamera;
+    [SerializeField] float slowMotionAmount = 0.2f;
+    private bool camEnabled = true;
+    private bool inSlowMotion = false;
+
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            ToggleCamera();
+        }
+    }
+
+    private void ToggleCamera()
+    {
+        if(camEnabled)
+        {
+            mainCamera.SetActive(false);
+            mainCamera.SetActive(true);
+            camEnabled = false;
+        }
+        else
+        {
+            mainCamera.SetActive(true);
+            mainCamera.SetActive(false);
+            camEnabled = true;
+        }
+    }
+
+    private void ToggleSlowMotion()
+    {
+
+    }
+}

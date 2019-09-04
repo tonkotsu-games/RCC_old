4f637d17331ac161f29beab15d26ef3c7bb5fbbb
Benjamin Rose <rose.benjamin@t-online.de>
2019-09-04 12:57:37 +0200

    Benjamin#055 EzySlice started

---
+++ RadicalAmp/Assets/Scripts/EzySlice/EnemySlice.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using EzySlice;
+
+public class EnemySlice : MonoBehaviour
+{
+    GameObject cube = null;
+    public Transform cutPlane;
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            SlicedHull hull = SliceObject(cube, null);
+            GameObject bottom = hull.CreateLowerHull(cube, null);
+            GameObject top = hull.CreateLowerHull(cube, null);
+            Destroy(cube);
+        }
+    }
+
+    public SlicedHull SliceObject(GameObject objectToSlice, Material crossSectionMaterial)
+    {
+        return objectToSlice.Slice(cutPlane.position, cutPlane.up, crossSectionMaterial);
+    }
+}

44d0f4197cc026d8d1fc98e6b78c6b0b6fee8154
Benjamin Rose <rose.benjamin@t-online.de>
2019-09-05 09:53:42 +0200

    EzySlice

--- RadicalAmp/Assets/Scripts/EzySlice/EnemySlice.cs
+++ RadicalAmp/Assets/Scripts/EzySlice/EnemySlice.cs
@@ -5,7 +5,7 @@ using EzySlice;
 
 public class EnemySlice : MonoBehaviour
 {
-    GameObject cube = null;
+    public GameObject cube = null;
     public Transform cutPlane;
 
     private void Update()
@@ -14,13 +14,28 @@ public class EnemySlice : MonoBehaviour
         {
             SlicedHull hull = SliceObject(cube, null);
             GameObject bottom = hull.CreateLowerHull(cube, null);
-            GameObject top = hull.CreateLowerHull(cube, null);
+            GameObject top = hull.CreateUpperHull(cube, null);
+            bottom.gameObject.transform.localScale = new Vector3(1.3f, 1.3f, 1.3f);
+            top.gameObject.transform.localScale = new Vector3(1.3f, 1.3f, 1.3f);
             Destroy(cube);
+            AddHullComponents(bottom);
+            AddHullComponents(top);
         }
     }
 
     public SlicedHull SliceObject(GameObject objectToSlice, Material crossSectionMaterial)
     {
         return objectToSlice.Slice(cutPlane.position, cutPlane.up, crossSectionMaterial);
+    }
+
+    public void AddHullComponents(GameObject go)
+    {
+        go.layer = 9;
+        Rigidbody rb = go.AddComponent<Rigidbody>();
+        rb.interpolation = RigidbodyInterpolation.Interpolate;
+        MeshCollider collider = go.AddComponent<MeshCollider>();
+        collider.convex = true;
+
+        rb.AddExplosionForce(100, go.transform.position, 20);
     }
 }
